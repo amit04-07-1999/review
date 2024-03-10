3a46f7635b085b4e61d2dc261b3d95410835b0cd
amit04-07-1999 <ameykumar76@gmail.com>
2024-03-10 15:18:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { StarIcon } from './IconComponents';
 
 function App() {
+  const [safetyRating, setSafetyRating] = useState(0);
+  const [communicationRating, setCommunicationRating] = useState(0);
+  const [recommendation, setRecommendation] = useState(null);
+
+  const handleSafetyRating = (rating) => {
+    setSafetyRating(rating);
+  };
+
+  const handleCommunicationRating = (rating) => {
+    setCommunicationRating(rating);
+  };
+
+  const handleRecommendation = (value) => {
+    setRecommendation(value);
+  };
+
+  const handleDeleteButtonClick = () => {
+    setSafetyRating(0);
+    setCommunicationRating(0);
+    setRecommendation(null);
+  };
+
+  const handleSubmitButtonClick = () => {
+    if (safetyRating === 0 || communicationRating === 0 || recommendation === null) {
+      alert('Your review is empty.');
+    } else {
+      alert('Your review has been submitted');
+      handleDeleteButtonClick();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="containe-1">
+        <h1>Leave a review</h1>
+      </div>
+      <div>
+        <h3 >Safety</h3>
+        <p >How safe did you feel with Trausti?</p>
+        <div >
+          {[1, 2, 3, 4, 5].map((index) => (
+            <StarIcon
+              key={index}
+              className={index <= safetyRating ? 'yellow' : 'gray'}
+              onClick={() => handleSafetyRating(index)}
+            />
+          ))}
+        </div>
+      </div>
+      <div >
+        <h3 >Communication</h3>
+        <p >How easy was to communicate with Trausti?</p>
+        <div >
+          {[1, 2, 3, 4, 5].map((index) => (
+            <StarIcon
+              key={index}
+              className={index <= communicationRating ? 'yellow' : 'gray'}
+              onClick={() => handleCommunicationRating(index)}
+            />
+          ))}
+        </div>
+      </div>
+      <div >
+        <h3 >Would you recommend Trausti?</h3>
+        <div >
+          <button className={`like ${recommendation === 'no' ? 'green white' : ' gray'}`} onClick={() => handleRecommendation('no')}>
+            No
+          </button>
+          <button className={` like ${recommendation === 'yes' ? 'green white' : 'gray'}`} onClick={() => handleRecommendation('yes')}>
+            Yes
+          </button>
+        </div>
+      </div>
+      <div >
+        <h3 >Praise</h3>
+        <p >What traits best describe Trausti?</p>
+        <div className="">
+          <button className="badge">Adventurous</button>
+          <button className="badge"> Clean</button>
+          <button className="badge">Good Listener</button>
+
+        </div>
+      </div>
+      <div >
+        <button className="delete" onClick={handleDeleteButtonClick}>
+          Delete
+        </button>
+        <button className="submit" onClick={handleSubmitButtonClick}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/IconComponents.js
@@ -0,0 +1,46 @@
+//svg taking from google
+
+
+import React from 'react';
+
+function PanelTopCloseIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+      <line x1="3" x2="21" y1="9" y2="9" />
+      <path d="m9 16 3-3 3 3" />
+    </svg>
+  );
+}
+
+function StarIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+    </svg>
+  );
+}
+
+export { PanelTopCloseIcon, StarIcon };
